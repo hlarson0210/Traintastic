0fa127b6a0b68f70572239c10a7feec705d2fd6c
Hayden Larson <hlarson398@gmail.com>
2019-08-13 20:24:14 -0500

    fixed firebase bug, can now add to database and pull info to fill table. Need to still calculate next arrival and minutes away

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ var config = {
     storageBucket: "",
     messagingSenderId: "54361661007",
     appId: "1:54361661007:web:227be2637419c583"
-};
+  };
 
 firebase.initializeApp(config);
 
@@ -19,23 +19,26 @@ $("#submit").on("click", function (event) {
     // Grabs user input
     var tName = $("#train-name").val().trim();
     var tDestination = $("#destination").val().trim();
-    var tTime = moment($("#train-time").val().trim(), "HH:MM").format("HH:MM");
+    var tTime = moment($("#train-time").val().trim(), "HHmm").format("HHmm");
     var tFrequency = $("#frequency").val().trim();
 
+    moment().format('LT');
+
     // Creates local "temporary" object for holding train data
-    var newEmp = {
+    var newTrain = {
         name: tName,
         destination: tDestination,
         time: tTime,
         frequency: tFrequency
     };
 
-    database.ref().push(newEmp);
+    // pushes newTrain object to database
+    database.ref().push(newTrain);
 
-    console.log(newEmp.name);
-    console.log(newEmp.destination);
-    console.log(newEmp.time);
-    console.log(newEmp.frequency);
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
 
     alert("Train schedule successfully added");
 
@@ -44,3 +47,28 @@ $("#submit").on("click", function (event) {
     $("#train-time").val("");
     $("#frequency").val("");
 });
+
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot);
+
+    var tName = childSnapshot.val().name;
+    var tDestination = childSnapshot.val().destination;
+    var tTime = childSnapshot.val().time;
+    var tFrequency = childSnapshot.val().frequency;
+
+    console.log(tName);
+    console.log(tDestination);
+    console.log(tTime);
+    console.log(tFrequency);
+
+    var empTimePretty = moment.unix(tTime).format("HH:mm");
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(tName),
+        $("<td>").text(tDestination),
+        $("<td>").text(tFrequency),
+        $("<td>").text(empTimePretty),
+    );
+
+    $("#employee-table > tbody").append(newRow);
+})
